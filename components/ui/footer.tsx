ff375ef79c7ec05de06f9461ae3a0e6ed22f636b
Fauzy <fachrulfauzy23@gmail.com>
2026-03-09 01:43:39 +0700

    feat: clear alamat

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -114,8 +114,9 @@ export default function Footer() {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.242-4.243a8 8 0 1111.314 0z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
-                                <span>Jl Dipanjaitan, KP Karangsari No 149<br />RT/RW 012/003 Soklat, Subang</span>
+                                {/* <span>Jl Dipanjaitan, KP Karangsari No 149<br />RT/RW 012/003 Soklat, Subang</span> */}
                             </li>
+                            
                         </ul>
                     </div>
                 </div>
